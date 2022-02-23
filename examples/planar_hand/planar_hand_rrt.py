867afddf51364826ff0815b4ff98c8ad03738ef9
Terry Suh <hjsuh94@gmail.com>
2022-02-23 17:52:54 -0500

    clean commit

--- examples/planar_hand/planar_hand_rrt.py
+++ examples/planar_hand/planar_hand_rrt.py
@@ -6,13 +6,19 @@ import pickle
 
 import cProfile
 
-from qsim.simulator import GradientMode
+from pydrake.all import PiecewisePolynomial
+
+from qsim.simulator import QuasistaticSimulator, GradientMode
+from qsim_cpp import QuasistaticSimulatorCpp
 
 from irs_mpc.quasistatic_dynamics import QuasistaticDynamics
+from irs_mpc.quasistatic_dynamics_parallel import (
+    QuasistaticDynamicsParallel)
+from irs_mpc.irs_mpc_quasistatic import (
+    IrsMpcQuasistatic)
 from irs_mpc.irs_mpc_params import IrsMpcQuasistaticParameters
 
 from irs_rrt.irs_rrt import IrsRrt, IrsNode
-from irs_rrt.irs_rrt_traj_opt import IrsRrtTrajOpt
 from irs_rrt.rrt_params import IrsRrtParams
 
 from planar_hand_setup import *
@@ -38,45 +44,22 @@ x0 = q_dynamics.get_x_from_q_dict(q0_dict)
 joint_limits = {
     idx_u: np.array([[-0.3, 0.3], [0.3, 0.5], [-0.01, np.pi]]),
     idx_a_l: np.array([[-np.pi / 2, np.pi / 2], [-np.pi / 2, 0]]),
-    idx_a_r: np.array([[-np.pi / 2, np.pi / 2], [0, np.pi / 2]])}
+    idx_a_r: np.array([[-np.pi / 2, np.pi / 2], [0, np.pi / 2]])
+}
 
 #%% RRT testing
-# IrsMpc params
-mpc_params = IrsMpcQuasistaticParameters()
-mpc_params.Q_dict = {
-    idx_u: np.array([20, 20, 10]),
-    idx_a_l: np.array([1e-3, 1e-3]),
-    idx_a_r: np.array([1e-3, 1e-3])}
-mpc_params.Qd_dict = {
-    model: Q_i * 100 for model, Q_i in mpc_params.Q_dict.items()}
-mpc_params.R_dict = {
-    idx_a_l: 10 * np.array([1, 1]),
-    idx_a_r: 10 * np.array([1, 1])}
-mpc_params.T = 20
-
-mpc_params.u_bounds_abs = np.array([
-    -np.ones(dim_u) * 2 * h, np.ones(dim_u) * 2 * h])
-
-mpc_params.calc_std_u = lambda u_initial, i: u_initial / (i ** 0.8)
-mpc_params.std_u_initial = np.ones(dim_u) * 0.3
-
-mpc_params.decouple_AB = True
-mpc_params.num_samples = 100
-mpc_params.bundle_mode = BundleMode.kFirst
-mpc_params.parallel_mode = ParallelizationMode.kCppBundledB
-
-# IrsRrt params
 params = IrsRrtParams(q_model_path, joint_limits)
 params.root_node = IrsNode(x0)
-params.max_size = 100
+params.max_size = 300
 params.goal = np.copy(x0)
 params.goal[6] = np.pi
 params.termination_tolerance = 1e-2
 params.goal_as_subgoal_prob = 0.5
 params.rewire = False
 params.distance_metric = 'local_u'
+
 # params.distance_metric = 'global'  # If using global metric
-params.global_metric = q_dynamics.get_x_from_q_dict(mpc_params.Q_dict)
+params.global_metric = np.array([0.1, 0.1, 0.1, 0.1, 10.0, 10.0, 1.0])
 
 
 irs_rrt = IrsRrt(params)
@@ -85,7 +68,9 @@ irs_rrt.iterate()
 #%%
 irs_rrt.save_tree(f"tree_{params.max_size}_planar_hand.pkl")
 
+
 #%%
+#
 # cProfile.runctx('tree.iterate()',
 #                  globals=globals(), locals=locals(),
 #                  filename='irs_rrt_profile.stat')
