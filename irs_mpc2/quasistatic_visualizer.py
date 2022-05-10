fe5119de17eeb68eb49d2847f9ca03cfad157911
Pang <pangtao@mit.edu>
2022-05-09 23:53:09 -0400

    Render a trajectory into images.

--- irs_mpc2/quasistatic_visualizer.py
+++ irs_mpc2/quasistatic_visualizer.py
@@ -1,9 +1,12 @@
+import os
 from typing import Dict, Set
 
 import numpy as np
 from pydrake.all import (ModelInstanceIndex, MultibodyPlant)
 from qsim.parser import (QuasistaticParser)
 from qsim_cpp import QuasistaticSimulatorCpp
+
+from pydrake.all import PiecewisePolynomial
 
 
 class QuasistaticVisualizer:
@@ -43,3 +46,35 @@ class QuasistaticVisualizer:
     def publish_trajectory(self, x_traj: np.ndarray, h: float):
         q_dict_traj = [self.q_sim.get_q_dict_from_vec(x) for x in x_traj]
         self.q_sim_py.animate_system_trajectory(h, q_dict_traj=q_dict_traj)
+
+    def normalize_quaternions_in_x(self, x: np.ndarray):
+        for model in self.q_sim_py.models_unactuated:
+            if not self.q_sim_py.is_3d_floating[model]:
+                continue
+            indices = self.q_sim_py.position_indices[model]
+            q = x[indices[:4]]
+            x[indices[:4]] /= np.linalg.norm(q)
+
+    def render_trajectory(self, x_traj_knots: np.ndarray, h: float,
+                          folder_path: str, fps: int = 60):
+        """
+        Saves rendered frames to folder_path.
+        """
+        n_knots = len(x_traj_knots)
+        t_knots = np.arange(n_knots) * h
+        x_traj = PiecewisePolynomial.FirstOrderHold(t_knots, x_traj_knots.T)
+
+        dt = 1 / fps
+        n_frames = int(t_knots[-1] / dt)
+
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+
+        for i in range(n_frames):
+            x = x_traj.value(dt * i).squeeze()
+            self.normalize_quaternions_in_x(x)
+            self.q_sim_py.update_mbp_positions_from_vector(x)
+            self.q_sim_py.draw_current_configuration(False)
+            im = self.q_sim_py.viz.vis.get_image()
+            im.save(os.path.join(folder_path, f"{i:04d}.png"), 'png')
+
