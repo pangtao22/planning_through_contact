fc8412ee06fc500d990eef1cf5b0039235264940
pangtao22 <pangtao22@users.noreply.github.com>
2022-05-16 15:54:06 -0700

    A analytic investigation and a new recording tool that saves images. (#56)
    * fix error in PR
    
    * Do not B[idx_a, :] to identity.
    
    * Minor update to tilted allegro hand example.
    
    * Debugging A.
    
    * Analytic A working!
    
    * add running examples with new interface
    
    * Some local changes.
    
    * Previous changes.
    
    * Render a trajectory into images.
    
    * Fix run_allegro_hand2 example
    
    * Show example of rendering images.
    
    * Damn! I can't find what makes the old IrsMpc better than the new...
    
    * Scheduling of log_barrier_weight in run_allegro_hand2 is off... but still different
    
    * Remove profanity.
    
    Co-authored-by: Terry Suh <hjsuh94@gmail.com>

--- examples/allegro_hand/run_allegro_hand2.py
+++ examples/allegro_hand/run_allegro_hand2.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-import os
 import time
-
+import matplotlib.pyplot as plt
 import numpy as np
-from irs_mpc2.irs_mpc import IrsMpcQuasistatic
-from irs_mpc2.irs_mpc_params import (SmoothingMode, IrsMpcQuasistaticParameters)
-from pydrake.all import (AngleAxis,
+
+from pydrake.all import (PiecewisePolynomial, RotationMatrix, AngleAxis,
                          Quaternion, RigidTransform)
 from pydrake.math import RollPitchYaw
 from pydrake.systems.meshcat_visualizer import AddTriad
-from qsim.model_paths import models_dir
+
 from qsim.parser import QuasistaticParser
-from qsim_cpp import ForwardDynamicsMode
+from qsim.simulator import QuasistaticSimulator, GradientMode
+from qsim_cpp import QuasistaticSimulatorCpp, GradientMode, ForwardDynamicsMode
 
-q_model_path = os.path.join(models_dir, 'q_sys',
-                            'allegro_hand_tilted_and_sphere.yml')
-robot_name = 'allegro_hand_right'
-object_name = 'sphere'
+from irs_mpc2.irs_mpc import IrsMpcQuasistatic
+from irs_mpc2.irs_mpc_params import (SmoothingMode, IrsMpcQuasistaticParameters)
 
-# %% sim setup
+from allegro_hand_setup import *
+
+#%% sim setup
 h = 0.1
 T = 20  # num of time steps to simulate forward.
 duration = T * h
+max_iterations = 10
 
 # quasistatic dynamical system
 q_parser = QuasistaticParser(q_model_path)
-q_parser.set_sim_params(gravity=[0, 0, -10])
+# q_parser.set_sim_params(gravity=[0, 0, -10])
 
 q_sim = q_parser.make_simulator_cpp()
 plant = q_sim.get_plant()
@@ -36,20 +36,20 @@ idx_a = plant.GetModelInstanceByName(robot_name)
 idx_u = plant.GetModelInstanceByName(object_name)
 
 # initial conditions.
-q_a0 = np.array([-0.03513728, 0.73406172, 0.64357553, 0.74325654, 0.58083794,
-                 0.96998129, 0.6349077, 0.8323073, -0.1095671, 0.70771197,
-                 0.64165158, 0.71923356, -0.04130878, 0.80228386, 0.83890058,
-                 0.90658696])
-q_u0 = np.array([0.99605745, 0.02259868, 0.08572997, -0.00303672, -0.09897396,
-                 0.00716867, 0.04708814])
+q_a0 = np.array([0.03501504, 0.75276565, 0.74146232, 0.83261002, 0.63256269,
+                 1.02378254, 0.64089555, 0.82444782, -0.1438725, 0.74696812,
+                 0.61908827, 0.70064279, -0.06922541, 0.78533142, 0.82942863,
+                 0.90415436])
+q_u0 = np.array([1, 0, 0, 0, -0.081, 0.001, 0.071])
+
 q0_dict = {idx_a: q_a0, idx_u: q_u0}
 
-# %%
+#%%
 params = IrsMpcQuasistaticParameters()
 params.h = h
 params.Q_dict = {
-    idx_u: np.array([10, 10, 10, 10, 10, 10, 10.]),
-    idx_a: np.ones(dim_u) * 1e-2}
+    idx_u: np.array([10, 10, 10, 10, 1, 1, 1.]),
+    idx_a: np.ones(dim_u) * 1e-3}
 
 params.Qd_dict = {}
 for model in q_sim.get_actuated_models():
@@ -63,16 +63,18 @@ u_size = 1.0
 params.u_bounds_abs = np.array([
     -np.ones(dim_u) * u_size * h, np.ones(dim_u) * u_size * h])
 
+
 params.smoothing_mode = SmoothingMode.kFirstAnalyticIcecream
 # sampling-based bundling
 params.calc_std_u = lambda u_initial, i: u_initial / (i ** 0.8)
-params.std_u_initial = np.ones(dim_u) * 0.2
+params.std_u_initial = np.ones(dim_u) * 0.3
 params.num_samples = 100
 # analytic bundling
 params.log_barrier_weight_initial = 100
-log_barrier_weight_final = 2000
+log_barrier_weight_final = 6000
 base = np.log(
-    log_barrier_weight_final / params.log_barrier_weight_initial) / T
+    log_barrier_weight_final / params.log_barrier_weight_initial) \
+       / max_iterations
 base = np.exp(base)
 params.calc_log_barrier_weight = (
     lambda kappa0, i: kappa0 * (base ** i))
@@ -82,9 +84,10 @@ params.rollout_forward_dynamics_mode = ForwardDynamicsMode.kSocpMp
 
 prob_mpc = IrsMpcQuasistatic(q_sim=q_sim, parser=q_parser, params=params)
 
-# %%
+
+#%%
 Q_WB_d = RollPitchYaw(0, 0, np.pi / 4).ToQuaternion()
-p_WB_d = q_u0[4:] + np.array([0, -0.01, 0], dtype=float)
+p_WB_d = q_u0[4:] + np.array([0, 0, 0], dtype=float)
 q_d_dict = {idx_u: np.hstack([Q_WB_d.wxyz(), p_WB_d]),
             idx_a: q_a0}
 x0 = q_sim.get_q_vec_from_dict(q0_dict)
@@ -94,14 +97,14 @@ x_trj_d = np.tile(xd, (T + 1, 1))
 u_trj_0 = np.tile(u0, (T, 1))
 prob_mpc.initialize_problem(x0=x0, x_trj_d=x_trj_d, u_trj_0=u_trj_0)
 
-# %%
+#%%
 t0 = time.time()
-prob_mpc.iterate(max_iterations=10, cost_Qu_f_threshold=1)
+prob_mpc.iterate(max_iterations=max_iterations, cost_Qu_f_threshold=1)
 t1 = time.time()
 
 print(f"iterate took {t1 - t0} seconds.")
 
-# %% visualize goal.
+#%% visualize goal.
 q_sim_py = prob_mpc.vis.q_sim_py
 AddTriad(
     vis=q_sim_py.viz.vis,
@@ -122,8 +125,8 @@ AddTriad(
 q_sim_py.viz.vis['goal'].set_transform(
     RigidTransform(Q_WB_d, p_WB_d).GetAsMatrix4())
 
-# %% results visualization.
-# Rollout trajectory according to the real physics.
+
+#%% Rollout trajectory according to the real physics.
 x_trj_to_publish = prob_mpc.rollout(
     x0=x0, u_trj=prob_mpc.u_trj_best, forward_mode=ForwardDynamicsMode.kSocpMp)
 
@@ -137,17 +140,10 @@ print('orientation error:',
       AngleAxis(Q_WB_f.multiply(Q_WB_d.inverse())).angle())
 print()
 
-# plot different components of the cost for all iterations.
+#%% plot different components of the cost for all iterations.
 prob_mpc.plot_costs()
 
-# %% save visualization.
+#%% save visualization.
 # res = q_dynamics.q_sim_py.viz.vis.static_html()
 # with open("allegro_hand_irs_lqr_60_degrees_rotation.html", "w") as f:
 #     f.write(res)
-
-#%%
-q_viz = prob_mpc.vis
-q_viz.render_trajectory(
-    x_traj_knots=x_trj_to_publish, h=h,
-    folder_path="/home/amazon/PycharmProjects/video_images")
-
