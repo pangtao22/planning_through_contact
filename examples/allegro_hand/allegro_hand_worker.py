a9a4f6f00cdf1103054b46ab660126321b1c6eff
Pang <pangtao@mit.edu>
2022-07-23 21:51:59 -0400

    Renaming and removing unused files.

--- examples/allegro_hand/allegro_hand_worker.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from irs_mpc.zmq_dynamics_worker import launch_workers
-
-from allegro_hand_setup import q_model_path, h
-
-if __name__ == "__main__":
-    launch_workers(q_model_path, h)
