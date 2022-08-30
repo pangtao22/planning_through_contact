90fbfe1aa5131e2b34c34be74e72bdfbc92b07e5
Pang <pangtao@mit.edu>
2022-08-30 14:50:37 -0400

    In, run_hardware, control is triggered by q_estimated.

--- examples/iiwa_bimanual/run_hardware.py
+++ examples/iiwa_bimanual/run_hardware.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import lcm
 from pydrake.all import PiecewisePolynomial
-from drake import lcmt_iiwa_command, lcmt_iiwa_status
+from drake import lcmt_iiwa_command, lcmt_iiwa_status, lcmt_scope
 
 from qsim.parser import QuasistaticParser
 
@@ -12,7 +12,7 @@ from control.drake_sim import load_ref_trajectories
 from control.systems_utils import wait_for_msg
 
 from iiwa_bimanual_setup import q_model_path
-
+from state_estimator import kQEstimatedChannelName
 
 #%%
 q_parser = QuasistaticParser(q_model_path)
@@ -22,12 +22,14 @@ h_ref_knot = 0.5
 q_knots_ref, u_knots_ref, t_knots = load_ref_trajectories(
     file_path="hand_trj.pkl", h_ref_knot=h_ref_knot, q_sim=q_sim)
 
-iiwa_status_msg = wait_for_msg(
-    "IIWA_STATUS", lcmt_iiwa_status, lambda msg: msg.num_joints == 14)
+q_msg = wait_for_msg(
+    kQEstimatedChannelName, lcmt_scope, lambda msg: msg.size == 21)
 
 t_knots += 10.0
 t_knots = np.hstack([0, t_knots])
-u_knots_ref = np.vstack([iiwa_status_msg.joint_position_measured, u_knots_ref])
+q = np.array(q_msg.value)
+q_a0 = q[q_sim.get_q_a_indices_into_q()]
+u_knots_ref = np.vstack([q_a0, u_knots_ref])
 u_ref_trj = PiecewisePolynomial.FirstOrderHold(
     t_knots, u_knots_ref.T)
 
@@ -36,15 +38,15 @@ first_status_msg_time = None
 
 
 def calc_iiwa_command(channel, data):
-    status_msg = lcmt_iiwa_status.decode(data)
+    q_msg = lcmt_scope.decode(data)
     global first_status_msg_time
     if first_status_msg_time is None:
-        first_status_msg_time = status_msg.utime / 1e6
+        first_status_msg_time = q_msg.utime / 1e6
 
-    t = status_msg.utime / 1e6 - first_status_msg_time
+    t = q_msg.utime / 1e6 - first_status_msg_time
     cmd_msg = lcmt_iiwa_command()
-    cmd_msg.utime = status_msg.utime
-    cmd_msg.num_joints = status_msg.num_joints
+    cmd_msg.utime = q_msg.utime
+    cmd_msg.num_joints = 14
     cmd_msg.joint_position = u_ref_trj.value(t).squeeze()
 
     lc.publish("IIWA_COMMAND", cmd_msg.encode())
@@ -52,12 +54,17 @@ def calc_iiwa_command(channel, data):
 
 lc = lcm.LCM()
 
-subscription = lc.subscribe("IIWA_STATUS", calc_iiwa_command)
+subscription = lc.subscribe(kQEstimatedChannelName, calc_iiwa_command)
 subscription.set_queue_capacity(1)
 
 try:
+    t_start = time.time()
     while True:
         lc.handle()
+        dt = time.time() - t_start
+        if dt > t_knots[-1] + 5:
+            break
+    print("Done!")
 
 except KeyboardInterrupt:
     pass
