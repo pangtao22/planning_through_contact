ad9a2d9cfda2e0f634c3aab85e50ca9a5a812fae
Terry Suh <hjsuh94@gmail.com>
2022-12-08 20:13:28 -0500

    mbp

--- examples/planar_pushing/planar_pushing_setup.py
+++ examples/planar_pushing/planar_pushing_setup.py
@@ -1,6 +1,10 @@
 import os
+import numpy as np
 
 from qsim.model_paths import models_dir
+from qsim_cpp import ForwardDynamicsMode, GradientMode
+
+from control.controller_system import ControllerParams
 
 q_model_path = os.path.join(models_dir, "q_sys", "box_pushing.yml")
 
@@ -17,3 +21,13 @@ num_samples = 100
 
 # data collection.
 data_folder = "ptc_data/planar_pushing"
+
+# Stabilization.
+controller_params = ControllerParams(
+    forward_mode=ForwardDynamicsMode.kLogIcecream,
+    gradient_mode=GradientMode.kBOnly,
+    log_barrier_weight=5000,
+    control_period=None,
+    Qu= np.diag([2, 2, 3]),
+    R=np.diag(0.1 * np.ones(2)),
+)
